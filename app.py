fc6a736f11aad78680a9ce21cfe6076dfd9ef40c
Roshu2 <kyumin1020@gmail.com>
2022-05-17 00:17:17 +0900

    1. db에 이메일, pw 저장

---
+++ app.py
@@ -0,0 +1,35 @@
+import hashlib
+import json
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from pymongo import MongoClient
+
+app = Flask(__name__)
+cors = CORS(app, resources={r'*': {'origins': '*'}})
+client = MongoClient('localhost', 27017)
+db = client.turtlegram
+
+@app.route("/")
+def hello_word():
+    return jsonify({'message':'success'})
+
+@app.route("/signup", methods=["POST"])
+def sign_up():
+    
+    data = json.loads(request.data)
+    
+    password_hash = hashlib.sha256(data['password'].encode('utf-8')).hexdigest()
+    
+    doc = {
+        'email' : data.get('email'),
+        'password' : password_hash
+    }
+    
+    db.users.insert_one(doc)
+    
+    return jsonify({'message':'success'})
+
+
+
+if __name__ =='__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
