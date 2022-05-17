89fca2a359425588c05ed265ddc5d2acf7a8ddc6
Roshu2 <kyumin1020@gmail.com>
2022-05-17 20:45:00 +0900

    1. 로그인 api 작성

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import hashlib
 import json
+import jwt
+from datetime import datetime, timedelta
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from pymongo import MongoClient
+
+SECRET_KEY = 'turtle'
 
 app = Flask(__name__)
 cors = CORS(app, resources={r'*': {'origins': '*'}})
@@ -28,6 +32,44 @@ def sign_up():
     db.users.insert_one(doc)
     
     return jsonify({'msg':'success'})
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    print(request)
+    data = json.loads(request.data)
+    print(data)
+    
+    email = data.get("email")
+    password = data.get("password")
+     # 회원가입 때와 같은 방법으로 pw를 암호화합니다.
+    hashed_pw = hashlib.sha256(password.encode('utf-8')).hexdigest()
+    print(hashed_pw)
+    
+    result = db.users.find_one({
+        'email': email, 
+        'password': hashed_pw
+    })
+    
+    if result:
+        print(result)
+       
+        
+        payload = {
+            'id': str(result["_id"]),
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)
+        }
+        token = jwt.encode(payload=payload, key=SECRET_KEY, algorithm='HS256')
+        print(payload)
+        print(token)
+    
+        return jsonify({'result': 'success', 'token': token})
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+    
+    
+    
+    
 
 
 
