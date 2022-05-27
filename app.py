0fdafe78d6774c6c2bf0ebf1eb86a1823d438b05
Roshu2 <kyumin1020@gmail.com>
2022-05-27 20:55:03 +0900

    1.거북이반 6강진행

--- app.py
+++ app.py
@@ -17,10 +17,10 @@ client = MongoClient('localhost', 27017)
 db = client.turtlegram
 
 
-#데코레이터
+#데코레이터 함수 로그인한 유저정보(토큰)을 불러오는 역할
 def authorize(f):
-    @wraps(f)
-    def decorated_function():
+    @wraps(f) 
+    def decorated_function(*args, **kwargs):     #*args 는 리스트형태로 인자가 얼마든지 들어와도된다 #**kwargs는 keyword 머머는 머 형태로 얼마든지 들어와도 인식을하겠다.
         if not 'Authorization' in request.headers:
             abort(401)
         token = request.headers['Authorization']
@@ -28,7 +28,7 @@ def authorize(f):
             user = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
         except: 
             abort(401)
-        return f(user)        
+        return f(user, *args, **kwargs)        
         
     return decorated_function
 
@@ -40,6 +40,7 @@ def hello_word(user):
     print(user)
     return jsonify({'msg':'success'})
 
+#회원가입
 @app.route("/signup", methods=["POST"])
 def sign_up():
     
@@ -56,7 +57,7 @@ def sign_up():
     
     return jsonify({'msg':'success'})
 
-
+#로그인
 @app.route("/login", methods=["POST"])
 def login():
     data = json.loads(request.data)
@@ -87,7 +88,7 @@ def login():
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
     
     
-    
+#유저 정보 불러오기
 @app.route("/getuserinfo", methods=["GET"])
 @authorize
 def get_user_info(user):
@@ -101,7 +102,7 @@ def get_user_info(user):
     
     return jsonify({"msg": "success", "email": result["email"]})
 
-
+#게시글 쓰기
 @app.route("/article", methods=["POST"])
 @authorize
 def post_article(user):
@@ -125,7 +126,7 @@ def post_article(user):
     
     return jsonify({"msg":"success"})
 
-
+#게시글 불러오기
 @app.route("/article", methods=["GET"])
 def get_article():
     articles = list(db.article.find())
@@ -134,7 +135,7 @@ def get_article():
 
     return jsonify({"msg":"success", "articles": articles})
 
-
+#게시글 상세페이지
 @app.route("/article/<article_id>", methods=["GET"])
 def get_article_detail(article_id):
     print(article_id)
@@ -143,6 +144,37 @@ def get_article_detail(article_id):
     article["_id"] = str(article["_id"])
     
     return jsonify({"msg":"success", "article": article})
+
+# 게시글 수정
+@app.route("/article/<article_id>", methods=["PATCH"])
+@authorize
+def patch_article_detail(user, article_id):
+    
+    data = json.loads(request.data)
+    title = data.get('title')
+    content = data.get('content')
+    
+    article = db.article.update_one({'_id': ObjectId(article_id), "user": user["id"]}, {
+        "$set": {"title": title, "content": content}})
+    print(article.matched_count)
+    
+    if article.matched_count:
+        return jsonify({"msg": "success"})
+    
+    return jsonify({"msg": "fail"}), 403
+
+#게시글 삭제
+@app.route("/article/<article_id>", methods=["DELETE"])
+@authorize
+def delete_article_detail(user, article_id):
+
+    article = db.article.delete_one(
+        {"_id": ObjectId(article_id), "user": user["id"]})
+
+    if article.deleted_count:
+        return jsonify({"message": "success"})
+    else:
+        return jsonify({"message": "fail"}), 403
     
 
 if __name__ =='__main__':
